579eca4012f3a4a12b2009be7f140e1ba5b600e1
Toby Nguyen <208221158+tobynguyen27@users.noreply.github.com>
2026-02-26 18:01:32 +0700

    refactor: clean code

--- apps/frontend/src/app/app.vue
+++ apps/frontend/src/app/app.vue
@@ -1,7 +1,5 @@
 <template>
-	<div class="">
-		<Header />
-		<NuxtPage />
-		<Footer />
-	</div>
+	<Header />
+	<NuxtPage />
+	<Footer />
 </template>
--- apps/frontend/src/app/pages/qr.vue
+++ apps/frontend/src/app/pages/qr.vue
@@ -17,6 +17,8 @@ const schema = type({
 	url: /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/,
 });
 
+const modal = useTemplateRef("modal");
+
 function generateQr() {
 	url.value = inputUrl.value;
 	modal.value?.showModal();
@@ -30,8 +32,6 @@ async function pasteUrl() {
 function clearUrl() {
 	inputUrl.value = "";
 }
-
-const modal = useTemplateRef("modal");
 
 function closeModal() {
 	modal.value?.close();
