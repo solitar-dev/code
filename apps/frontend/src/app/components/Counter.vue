02df8f3dcf12487c21efe07d0376e9ae39ec3bc2
Toby Nguyen <208221158+tobynguyen27@users.noreply.github.com>
2026-02-27 18:40:04 +0700

    chore: fetch statistic data for counter

--- apps/frontend/src/app/components/Counter.vue
+++ apps/frontend/src/app/components/Counter.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
-const totalLinks = 0;
-const totalClicks = 0;
+const { $api } = useNuxtApp();
+const statisticRepo = repository($api);
+
+const { pending, data } = await useAsyncData(() => statisticRepo.getStatisticData());
+
+const totalLinks = computed(() => data.value?.totalLinks ?? 0);
+const totalClicks = computed(() => data.value?.totalClicks ?? 0);
 </script>
 
 <template>
 	<div class="">
-		<p class="font-semibold text-lg">
+		<div class="w-full bg-gray-200 h-7 rounded-lg" v-if="pending" />
+		<p class="font-semibold text-lg" v-else>
 			<span class="text-2xl text-purple">{{ totalLinks }}</span> links created that have been
 			accessed <span class="text-2xl text-purple">{{ totalClicks }}</span> times
 		</p>
