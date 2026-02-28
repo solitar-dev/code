5623bde7e9fe83a321978f3baa701571eb5fe343
Toby Nguyen <208221158+tobynguyen27@users.noreply.github.com>
2026-02-28 09:15:02 +0700

    fix: og image

--- apps/frontend/src/app/components/OgImage/Basic.takumi.vue
+++ apps/frontend/src/app/components/OgImage/Basic.takumi.vue
@@ -12,11 +12,11 @@ defineProps<Props>();
 		<img src="/og_template.png" alt="og" class="absolute" width="1200px" height="630px" />
 
 		<div class="relative flex flex-col justify-between my-12 mx-20">
-			<h1 class="text-7xl text-[#EDEDED] font-brand">Solitar</h1>
+			<h1 class="text-7xl text-[#EDEDED]" :style="{ fontFamily: 'Orbitron' }">Solitar</h1>
 
-			<div class="flex flex-col">
-				<h2 class="text-5xl text-[#EDEDED] font-mono" v-if="subtitle">{{ subtitle }}</h2>
-				<h3 v-if="description" class="text-[#A0A0A0] text-3xl font-mono">
+			<div class="flex flex-col" :style="{ fontFamily: 'Geist Mono' }">
+				<h2 class="text-5xl text-[#EDEDED]" v-if="subtitle">{{ subtitle }}</h2>
+				<h3 v-if="description" class="text-[#A0A0A0] text-3xl">
 					{{ description }}
 				</h3>
 			</div>
