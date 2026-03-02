abe1b2bc7b7355142f633b2383e07f71c2a335f3
Toby Nguyen <208221158+tobynguyen27@users.noreply.github.com>
2026-03-02 10:35:43 +0700

    feat(frontend): add error page

---
+++ apps/frontend/src/app/error.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import type { NuxtError } from "#app";
+
+const status = computed(() => props.error?.status || 500);
+
+const props = defineProps({
+	error: Object as () => NuxtError,
+});
+</script>
+
+<template>
+	<Header />
+	<div class="h-screen grid place-items-center">
+		<div class="font-mono text-center">
+			<h1 class="text-9xl">{{ status }}</h1>
+			<h2 class="text-2xl">{{ error?.statusText }}</h2>
+		</div>
+	</div>
+	<Footer />
+</template>
