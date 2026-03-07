563fa4bfdcf3c4fabb68a0707e733fffbf0b9821
Toby Nguyen <208221158+tobynguyen27@users.noreply.github.com>
2026-03-07 16:34:12 +0700

    feat(ui): add Kbd component

---
+++ packages/ui/src/components/base/Kbd.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+type Props = {
+	label?: string;
+};
+
+withDefaults(defineProps<Props>(), { label: "" });
+</script>
+
+<template>
+	<kbd class="border border-default bg-surface rounded-lg px-1">{{ label }}</kbd>
+</template>
---
+++ packages/ui/src/stories/base/Kbd.stories.ts
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from "@storybook/vue3-vite";
+import Kbd from "../../components/base/Kbd.vue";
+
+const meta = {
+	title: "Base/Kbd",
+	component: Kbd,
+} satisfies Meta<typeof Kbd>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Letter: Story = {
+	args: {
+		label: "s",
+	},
+};
+
+export const Character: Story = {
+	args: {
+		label: ".",
+	},
+};
